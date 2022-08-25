405ad26ff6663d741d5585d6305666d2bc09c3a0
xyzpda <ace1231hk@gmail.com>
2022-08-25 17:50:48 +0900

    add ABC197 A cs

---
+++ atcorder/ABC197/A/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class A
+{
+    static void Main(string[] args)
+    {
+        var result = Console.ReadLine();
+        Console.WriteLine(result[1].ToString() + result[2].ToString() + result[0].ToString());
+    }
+}
