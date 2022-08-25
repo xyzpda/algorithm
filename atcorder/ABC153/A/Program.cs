e0befb7a7ff1471cab921102afd7a89f43f61fcc
xyzpda <ace1231hk@gmail.com>
2022-08-25 17:24:30 +0900

    add ABC153 A

---
+++ atcorder/ABC153/A/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var result = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+Console.WriteLine(result[0]);
+Console.WriteLine(result[1]);
